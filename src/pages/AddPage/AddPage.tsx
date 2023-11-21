55f92a22b3c3c569e1473fafa047c51b9678c736
Patryk Kamusiński <theneonxd@gmail.com>
2023-11-21 01:53:48 +0100

    handle add page form

--- src/pages/AddPage/AddPage.tsx
+++ src/pages/AddPage/AddPage.tsx
@@ -1,19 +1,71 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './AddPage.scss';
-import cameraIcon from '../../images/camera.svg';
 
 export default function AddPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    country: '',
+    time: '',
+    photo: null,
+    method: '',
+    ingredients: '',
+  });
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleChange = (e: any) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleFileChange = (e: any) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      photo: file,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (
+      !(
+        formData.name &&
+        formData.country &&
+        formData.time &&
+        formData.ingredients &&
+        formData.method
+      )
+    ) {
+      setErrorMsg('Fill all required fields first');
+      return;
+    }
+    setErrorMsg('');
+    console.log(formData);
+  };
+
   return (
     <div className='add-page'>
       <div className='add-food-form'>
         <div className='inputs-section'>
           <div>
             <label htmlFor='name'>Dishes name</label>
-            <input type='text' id='name' />
+            <input
+              type='text'
+              id='name'
+              value={formData.name}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor='country'>Country</label>
-            <select name='' id='country' className='custom-select'>
+            <select
+              id='country'
+              className='custom-select'
+              value={formData.country}
+              onChange={handleChange}
+            >
               <option value='' disabled></option>
               <option value='poland'>Poland</option>
               <option value='japan'>Japan</option>
@@ -25,7 +77,12 @@ export default function AddPage() {
           </div>
           <div>
             <label htmlFor='time'>Time</label>
-            <select name='' id='time' className='custom-select'>
+            <select
+              id='time'
+              className='custom-select'
+              value={formData.time}
+              onChange={handleChange}
+            >
               <option value='' disabled></option>
               <option value='0.12'>5 min</option>
               <option value='0.25'>15 min</option>
@@ -35,24 +92,43 @@ export default function AddPage() {
               <option value='3'>3h+</option>
             </select>
           </div>
-          <div>
+          <div className='photo-input'>
             <label htmlFor='photo'>Photo</label>
-            <input type='text' id='photo' />
+            <div className='photo-container'>
+              <input
+                type='file'
+                id='photo'
+                onChange={handleFileChange}
+                accept='image/*'
+                className='photo-input'
+              />
+            </div>
           </div>
         </div>
         <div className='textarea-section'>
           <div>
             <label htmlFor='method'>Method</label>
-            <textarea name='method' id='method'></textarea>
+            <textarea
+              id='method'
+              value={formData.method}
+              onChange={handleChange}
+            ></textarea>
           </div>
           <div>
             <label htmlFor='ingredients'>Ingredients</label>
-            <textarea name='ingredients' id='ingredients'></textarea>
+            <textarea
+              id='ingredients'
+              value={formData.ingredients}
+              onChange={handleChange}
+            ></textarea>
           </div>
         </div>
       </div>
       <div style={{ marginTop: 50 }}>
-        <button className='custom-button'>Save</button>
+        <button className='custom-button' onClick={handleSubmit}>
+          Save
+        </button>
+        <div style={{ color: 'red' }}>{errorMsg}</div>
       </div>
     </div>
   );
